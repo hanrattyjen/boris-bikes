931d45c7426d44b950a0de590382a57d4ad291f8
hanrattyjen <hanratty.jen@gmail.com>
2016-09-27 16:37:53 +0100

    Added dock_bike method to docking_station.rb.
    Added dock bikes spec to spec file.

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,7 +1,12 @@
 require_relative 'bike'
 
 class DockingStation
+	attr_reader :bike
 	def release_bike
 		Bike.new
+	end
+	def dock(bike)
+		# use an instance variable to store the bike in the 'state' of this instance
+		@bike = bike
 	end
 end
